a73702f5fa7e70f9c7f502e8d127f57228e99f76
Tim Millard <timwmillard@gmail.com>
2026-06-25 17:48:11 +1000

    clang formatting base_raylib

--- base_raylib/src/main.c
+++ base_raylib/src/main.c
@@ -3,17 +3,18 @@
 #include "raylib.h"
 
 int main(void) {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+   InitWindow(800, 450, "raylib [core] example - basic window");
 
-    SetTargetFPS(60);
+   SetTargetFPS(60);
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
-    }
-    CloseWindow();
+   while (!WindowShouldClose()) {
+      BeginDrawing();
+      ClearBackground(RAYWHITE);
+      DrawText("Congrats! You created your first window!", 190, 200, 20,
+               LIGHTGRAY);
+      EndDrawing();
+   }
+   CloseWindow();
 
-    return 0;
+   return 0;
 }
