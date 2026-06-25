58e2300cde5ecaf449a38b3b276ae9e47a2d63c8
Tim Millard <timwmillard@gmail.com>
2026-06-25 14:56:07 +1000

    static webserver

--- base_ecewo/src/main.c
+++ base_ecewo/src/main.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
+#include "ecewo-static.h"
 #include "ecewo.h"
 #include "slog.h"
 
-void hello_world(ecewo_request_t *req, ecewo_response_t *res) {
-   slog_info("Hello, World!");
-   ecewo_send_text(res, ECEWO_OK, "Hello, World!");
-}
-
 int main(void) {
-
    slog_handler *handler = slog_color_text_handler_new(stdout, SLOG_INFO);
    slog_logger *logger = slog_new(handler);
    slog_set_default(logger);
-
    ecewo_app_t *app = ecewo_create();
-   if (!app) {
-      fprintf(stderr, "Failed to initialize server\n");
-      return -1;
+   ecewo_static_init();
+
+   // Serve ./public at the root URL
+   if (ecewo_serve_static(app, "/", "./public", NULL) != 0) {
+      fprintf(stderr, "Failed to mount static directory\n");
+      return 1;
    }
 
-   ECEWO_GET(app, "/", hello_world);
+   /*
+    * GET /               -> ./public/index.html
+    * GET /about.html     -> ./public/about.html
+    * GET /css/style.css  -> ./public/css/style.css
+    * GET /js/app.js      -> ./public/js/app.js
+    * GET /img/logo.png   -> ./public/img/logo.png
+    */
 
-   if (ecewo_listen(app, 3000) != 0) {
-      fprintf(stderr, "Failed to start server\n");
-      return -1;
-   }
+   slog_info("Server starting");
 
+   // ecewo_atexit(app, ecewo_static_cleanup, NULL);
+   ecewo_listen(app, 3000);
    return 0;
 }
