c1ff6bf9ddca3da6fe53568c8c10acff3ed0a059
Tim Millard <timwmillard@gmail.com>
2026-06-28 19:40:45 +1000

    remvoe duplicate types

--- base/base.h
+++ base/base.h
@@ -14,10 +14,6 @@
 #include <stdlib.h>
 
 typedef uint8_t u8;
-typedef size_t usize;
-typedef ptrdiff_t isize;
-
-typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
