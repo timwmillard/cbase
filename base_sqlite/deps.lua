1a73a411ae2ed99bf568e706a39863b76df7e596
Tim Millard <timwmillard@gmail.com>
2026-06-30 11:16:20 +1000

    deps: not working

--- base_sqlite/deps.lua
+++ base_sqlite/deps.lua
@@ -5,6 +5,14 @@ return {
         files = { 'lib/base.h' },
     },
     {
+        'timwmillard/cbase',
+        files = {
+            'tool/bin2c/bin2c.c',
+            'tool/sql2c/sql2c.c',
+            'tool/sql2c/CMakeLists.txt',
+        },
+    },
+    {
         'https://sqlite.org/2026/sqlite-amalgamation-3530300.zip',
         files = { 'sqlite3.c', 'sqlite3.h' },
     },
